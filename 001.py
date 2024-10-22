04df92727a6a15d37f35c6a7bb1ceaef406df9cc
caraxes512 <srivastava.arpit2024@gmail.com>
2024-10-22 22:05:45 +0530

    Update 001.py

--- 001.py
+++ 001.py
@@ -21,16 +21,13 @@ print(ordinal_encoding(data))
 
 
 import numpy as np
-def onehot_encode(data1):
+def onehot(data1):
     unique_categories = np.unique(data1)
 
-    # Create a dictionary mapping categories to indices
     category_to_index = {category: i for i, category in enumerate(unique_categories)}
 
-    # Create an empty one-hot encoded matrix (number of rows = len(data), number of columns = len(unique_categories))
     one_hot_encoded = np.zeros((data1.shape[0], len(unique_categories)), dtype=int)
 
-    # Populate the one-hot encoded matrix
     for i, category in enumerate(data1):
         one_hot_encoded[i, category_to_index[category]] = 1
 
@@ -38,7 +35,7 @@ def onehot_encode(data1):
 
 data = np.array(['A', 'B', 'C', 'D', 'E', 'F'])
 
-encoded_data, encoding_map = onehot_encode(data)
+encoded_data, encoding_map = onehot(data)
 
 print("Original Data:", data)
 print("One-Hot Encoded Data:\n", encoded_data)
