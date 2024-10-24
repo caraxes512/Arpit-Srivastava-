8056088f5748d2b7aa27d941d7d3b35c1fb8cb52
caraxes512 <srivastava.arpit2024@gmail.com>
2024-10-24 13:24:20 +0530

    Update 001.py

--- 001.py
+++ 001.py
@@ -31,6 +31,30 @@ def onehot(data1):
     for i, category in enumerate(data1):
         one_hot_encoded[i, category_to_index[category]] = 1
 
+
+
+
+def gradient_descent(x,y):
+    m=b=0
+    learning_rate=0.001
+    iterations=1000
+    n=len(x)
+    for i in range(iterations):
+        y_predicted=m*x+b
+        cost=(1/n)*sum([val**2 for val in(y-y_predicted)])
+        md=-(2/n)*sum(x*(y-y_predicted))
+        bd=-(2/n)*sum(y-y_predicted)
+
+        m-=learning_rate*md
+        b-=learning_rate*bd
+
+        print("m:",m,"b:",b,"iterations:",i,"cost:",cost)
+
+x=np.array([1,2,3,4,5])
+y=np.array([5,7,9,11,13])
+gradient_descent(x,y)
+    
+
     return one_hot_encoded, category_to_index
 
 data = np.array(['A', 'B', 'C', 'D', 'E', 'F'])
